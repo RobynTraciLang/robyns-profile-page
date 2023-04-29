64db09050c51f9a5475e6dfaddbccac5c87fca95
RobynTraciLang <124897590+RobynTraciLang@users.noreply.github.com>
2023-04-28 22:32:05 -0500

    Profile Page My Way, with a little JS Practice

---
+++ Lang_Robyn_ProfilePage - My Way/script.js
@@ -0,0 +1,13 @@
+// let userCardTopBar = document.querySelector(".user-card-top-bar");
+
+function changeColor(element) {
+    element.style.backgroundColor = "aqua";
+}
+
+function changeBack(element) {
+    element.style.backgroundColor = "fuchsia";
+}
+
+console.log("I'm working")
+
+
