337bd31c06f0a27141d9c679793741570bc26bd3
RobynTraciLang <124897590+RobynTraciLang@users.noreply.github.com>
2023-04-28 23:55:59 -0500

    I really like this!

--- Lang_Robyn_ProfilePage - My Way/script.js
+++ Lang_Robyn_ProfilePage - My Way/script.js
@@ -1,7 +1,7 @@
 // let userCardTopBar = document.querySelector(".user-card-top-bar");
 
 function changeColor(element) {
-    element.style.backgroundColor = "aqua";
+    element.style.backgroundColor = "rgb(196, 6, 196)";
 }
 
 function changeBack(element) {
@@ -10,4 +10,20 @@ function changeBack(element) {
 
 console.log("I'm working")
 
+function activateDarkMode(element) {
+    element.style.backgroundColor = "#211111";
+    element.style.color = "#ffffff";
+}
+//code I tried from the Learn Platform...it
 
+// let darkMode = activateDarkMode(element)
+
+// function activateDarkMode(element) {
+//     if(element.classList.includes("dark-mode")) {
+//         element.innerText = "Activate Light Mode";
+//         element.classList.remove("dark-mode");
+//     } else {
+//         element.innerText = "Activate Dark Mode";
+//         element.classList.add("dark-mode");
+//     }
+// }
